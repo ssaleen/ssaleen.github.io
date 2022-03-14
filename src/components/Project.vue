5755a28b3d66bed71c8fff5b247a72d54b56a333
Saleen Shahriar <saky0027@gmail.com>
2022-03-13 22:14:48 -0400

    project component

---
+++ src/components/Project.vue
@@ -0,0 +1,22 @@
+<template>
+  <h3>{{ title }}</h3>
+  <p>{{ skills.join(", ") }} | {{ date }}</p>
+</template>
+
+<script>
+export default {
+  name: "Project",
+  props: {
+    title: String,
+    skills: Array,
+    date: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+  margin-top: 0%;
+}
+</style>
