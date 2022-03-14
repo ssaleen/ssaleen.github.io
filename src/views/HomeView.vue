34fa71d43eadda7c81f5fb2544dae59d38ca74db
Saleen Shahriar <saky0027@gmail.com>
2022-03-14 00:18:04 -0400

    home change

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@
       Hi, I'm Saleen, a second year Computer Engineering student at the 
       <a href="https://uwaterloo.ca/" target="_blank">University of Waterloo</a>.
       I enjoy learning about all things technology, programming, and electronics!
-      If you don't like that stuff, I also love soccer, F1, and basketball!
+      If you don't like that stuff, I also love soccer, basketball, biking, and F1!
     </p>
     <p>
       You can check out my
