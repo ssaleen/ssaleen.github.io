3f0d292481e647a40f0e55ff75c144f15b472326
Saleen-S <saky0027@gmail.com>
2022-03-25 23:24:25 -0400

    removed hash history

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const DEFAULT_TITLE = 'Saleen Shahriar';
@@ -66,7 +66,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
